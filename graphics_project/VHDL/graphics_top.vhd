b648d83d20e0576278ec7b4c85c5f80b77ba80bb
Marijn Adriaanse <eismquantum2@gmail.com>
2021-11-19 09:33:50 +0100

    Changed inputs/outputs Ignore non-code project files

--- graphics_project/VHDL/graphics_top.vhd
+++ graphics_project/VHDL/graphics_top.vhd
@@ -4,8 +4,34 @@ use IEEE.std_logic_1164.ALL;
 entity graphics_top is
    port(clk    : in  std_logic;
         reset  : in  std_logic;
+
         h_sync : out std_logic;
         v_sync : out std_logic;
-        colour : out std_logic);
+	
+	colorA0: out std_logic;
+	colorA1: out std_logic;
+	colorB0: out std_logic;
+	colorB1: out std_logic;
+
+	incrx  : out std_logic;
+	incry  : out std_logic;
+	memrst : out std_logic;
+	data   : in  std_logic_vector(7 downto 0);
+
+	borders : in std_logic_vector(7 downto 0);
+	jumps	: in std_logic_vector(7 downto 0);
+
+	player0_pos   : in std_logic_vector(10 downto 0);
+	player1_pos   : in std_logic_vector(10 downto 0);
+	player0_dir   : in std_logic_vector(1 downto 0);
+	player1_dir   : in std_logic_vector(1 downto 0);
+	player0_state : in std_logic_vector(1 downto 0);
+	player1_state : in std_logic_vector(1 downto 0);
+
+	busy   : out std_logic;
+
+	game_state : in std_logic_vector(2 downto 0)
+
+        );
 end graphics_top;
 
