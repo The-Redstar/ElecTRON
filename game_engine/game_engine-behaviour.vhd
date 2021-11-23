1b9f74dc5125b8b926ad53827d4411a053cffcc8
Willem van Overbeeke <w.j.vanoverbeeke@student.tudelft.nl>
2021-11-23 11:46:28 +0100

    calculating next position met register gefikst

--- game_engine/game_engine-behaviour.vhd
+++ game_engine/game_engine-behaviour.vhd
@@ -530,8 +530,8 @@ create_next_state: 	process (state)
 				busy_counter_reset			<= '0';
 				clear_memory				<= '0';
 				
-				e_position_0				<= '1';
-				e_position_1				<= '1';
+				e_position_0				<= '0';
+				e_position_1				<= '0';
 				e_read_memory_0				<= '0';
 				e_read_memory_1				<= '0';
 				e_direction_0				<= '0';
@@ -623,25 +623,38 @@ create_next_state: 	process (state)
 					d_wallshape_1 <= (others => '0');
 				end if;
 	
-				--register signals fiksen
+				e_next_position_0 <= '1';
 				if (direction_0 = "01") then 		-- moves to the left, x is decreased with 1
-					d_next_position_0(4 downto 0) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) - 1, 5));
+					d_next_position_0(4 downto 0)  <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) - 1, 5));
+					d_next_position_0(10 downto 5) <= position_0(10 downto 5);
 				elsif (direction_0 = "11") then 	-- moves to the right, x is increased with 1
-					next_position_0(4 downto 0) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) + 1, 5));
+					d_next_position_0(4 downto 0)  <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) + 1, 5));
+					d_next_position_0(10 downto 5) <= position_0(10 downto 5);
 				elsif (direction_0 <= "00") then 	-- moves up, y is decreased with 1
-					next_position_0(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) - 1, 5));
+					d_next_position_0(4 downto 0) <= position_0(4 downto 0);
+					d_next_position_0(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) - 1, 5));
+					d_next_position_0(10)		  <= position_0(10);
 				else 					--moves down, y is increased with 1
-					next_position_0(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) + 1, 5));	
+					d_next_position_0(4 downto 0) <= position_0(4 downto 0);
+					d_next_position_0(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) + 1, 5));
+					d_next_position_0(10)		  <= position_0(10)	;	
 				end if;
 
+				e_next_position_1 <= '1';
 				if (direction_1 = "01") then 		-- moves to the left, x is decreased with 1
-					next_position_1(4 downto 0) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) - 1, 5));
+					d_next_position_1(4 downto 0)  <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) - 1, 5));
+					d_next_position_1(10 downto 5) <= position_1(10 downto 5);
 				elsif (direction_1 = "11") then 	-- moves to the right, x is increased with 1
-					next_position_1(4 downto 0) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) + 1, 5));
+					d_next_position_1(4 downto 0)  <= std_logic_vector(to_unsigned(to_integer(unsigned(position_0(4 downto 0))) + 1, 5));
+					d_next_position_1(10 downto 5) <= position_1(10 downto 5);
 				elsif (direction_1 <= "00") then 	-- moves up, y is decreased with 1
-					next_position_1(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) - 1, 5));
+					d_next_position_1(4 downto 0) <= position_1(4 downto 0);
+					d_next_position_1(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) - 1, 5));
+					d_next_position_1(10)		  <= position_1(10)	;	
 				else 					--moves down, y is increased with 1
-					next_position_1(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) + 1, 5));	
+					d_next_position_1(4 downto 0) <= position_1(4 downto 0);
+					d_next_position_1(9 downto 5) <= std_logic_vector(to_unsigned(to_integer(unsigned(position_1(9 downto 5))) + 1, 5));	
+					d_next_position_1(10)		  <= position_1(10)	;	
 				end if;
 				
 				new_state <= check_border;
