e1f91042659e0d567fb66b5a8aecd39361f3aa6b
Dagmar Westenbrink <d.r.m.westenbrink@student.tudelft.nl>
2021-11-25 11:57:34 +0100

    improved version after checking testbench

--- game_engine/game_engine-behaviour.vhd
+++ game_engine/game_engine-behaviour.vhd
@@ -340,11 +340,12 @@ create_next_state: 	process (state, reset, input, busy, read_memory, memory_read
 				end if;
 				
 			when get_ready =>
-				state_vga 					<= "010";
+				state_vga 					<= "000";
 				write_enable 				<= '0';
 				write_memory 				<= "00000000";
 				address 					<= "0000000000";
 				go_to						<= '0';
+				clear_memory				<= '0';
 				
 				e_position_0				<= '0';
 				e_position_1				<= '0';	
@@ -1224,7 +1225,7 @@ create_next_state: 	process (state, reset, input, busy, read_memory, memory_read
 				write_memory(7 downto 3) 	<= "00001" ;
 				write_memory(2 downto 0) 	<= wallshape_1;
 				address 					<= position_1(9 downto 0);
-				go_to 						<= '1';
+				go_to 						<= '0';
 				busy_counter_reset			<= '0';
 				clear_memory				<= '0';
 				
@@ -1270,7 +1271,7 @@ create_next_state: 	process (state, reset, input, busy, read_memory, memory_read
 				write_enable 				<= '0';
 				write_memory				<= "00000000";
 				address 					<= "0000000000";
-				go_to 						<= '1';
+				go_to 						<= '0';
 				busy_counter_reset			<= '0';
 				clear_memory				<= '0';
 				
