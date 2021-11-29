6e231448132eb21199b24d5a75103b27de971f65
Sander Britton <s.britton@student.tudelft.nl>
2021-11-29 12:19:17 +0100

    Added more code for the layering and actual drawing, but this code is far from complete (and efficient) so multiple discussion points for 30-11

--- graphics_project/VHDL/pixelator.vhd
+++ graphics_project/VHDL/pixelator.vhd
@@ -36,6 +36,20 @@ signal bbox_h, bbox_v: std_logic;
 signal bbox_explosion_inner, bbox_explosion_outer: std_logic;
 -------------------------------------------------------------
 signal layer: std_logic_vector(3 downto 0);
+signal wall_1_N, wall_1_S, wall_1_E, wall_1_W, wall_0_N, wall_0_S, wall_0_E, wall_0_W: std_logic;
+
+constant C_explosion_outer 			: std_logic_vector(3 downto 0):= "0000";
+constant C_explosion_inner 			: std_logic_vector(3 downto 0):= "0000";
+constant C_player0 				: std_logic_vector(3 downto 0):= "0000";
+constant C_player1 				: std_logic_vector(3 downto 0):= "0000";
+constant C_wall1 				: std_logic_vector(3 downto 0):= "0000";
+constant C_wall2 				: std_logic_vector(3 downto 0):= "0000";
+constant C_jump0 				: std_logic_vector(3 downto 0):= "0000";
+constant C_jump1 				: std_logic_vector(3 downto 0):= "0000";
+constant C_border0 				: std_logic_vector(3 downto 0):= "0000";
+constant C_border1 				: std_logic_vector(3 downto 0):= "0000";
+constant C_dot	 				: std_logic_vector(3 downto 0):= "0000";
+constant C_background				: std_logic_vector(3 downto 0):= "0000";
 
 begin
 LUT:	process(dx, dy)-- the needed conditions for making the bounding boxes
@@ -214,26 +228,58 @@ bounds:	process(xe0, xe1, xg1, xg3, xg4, xg5, xg6, xs9, xs10, xs11, xs12, xs14, 
 		bbox_explosion_inner<= (xg3 and xs12) and (yg3 and ys12) and rounding;
 	end process;
 
-layering: process(bbox_wall, bbox_dot, bbox_v, bbox_h, bbox_border, bbox_jump, bbox_explosion_outer, bbox_explosion_inner)
+layering: process(bbox_wall, bbox_dot, bbox_v, bbox_h, bbox_border, bbox_jump, bbox_explosion_outer, bbox_explosion_inner, walls, borders, jumps, player0_dir, player0_en, player0_layer, player0_mode, player1_dir, player1_en, player1_layer, player1_mode)
 	begin
-		if ((bbox_explosion_outer or bbox_explosion_inner) and (player1_mode(1) or player0_mode(1)) then
-			layer <= "1001";
-			--colouring stuff
-		elsif ((bbox_border(4) and borders(4)) or (bbox_border(5) and borders(5)) or (bbox_border(6) and borders(6)) or (bbox_border(7) and borders(7)) then
-			layer <= "1000";
-			--colouring stuff
-		elsif ((bbox_h and (player0_dir(0) or player1_dir(0))) or (bbox_v and (not(player0_dir(0) or not(player1_dir(0)))))) then 
-			layer <= "0111";
-			--colouring stuff
-		elsif (bbox_wall(4) or bbox_wall(5) or bbox_wall(6) or bbox_wall(7)) then
-			layer <= "0110";
-		elsif (bbox_jump(0) or bbox_jump(1) or bbox_jump(2) or bbox_jump(3)) then
-			layer <= "0101";
-		elsif (bbox_border(0) or bbox_border(1) or bbox_border(2) or bbox_border(3)) then
-			layer <= "0100";
-		elsif ((bbox_h and (player0_dir(0) or player1_dir(0))) or (bbox_v and (not(player0_dir(0) or not(player1_dir(0)))))) then
-			layer <= "0011";
+		if ((walls(6 downto 4) = "010") or (walls(6 downto 4) = "100") or (walls(6 downto 4) = "111")) then
+			wall_1_N <= '1';
+		else
+			wall_1_N <= '0';
+		end if; -- maybe make this into a LUT and maybe put all of this in a seperate process???
+		
+		if ((bbox_explosion_outer or bbox_explosion_inner) and (not(player1_mode(1)) or not(player0_mode(1)))) = '1' then
+			layer <= "1001"; --explosion layer
+			if (bbox_explosion_outer = '1') then -- could be expanded by making different explosion colours for different players
+				colour <= C_explosion_outer;
+			else
+				colour <= C_explosion_inner;
+			end if;
+		elsif ((bbox_border(0) and borders(4)) or (bbox_border(1) and borders(5)) or (bbox_border(2) and borders(6)) or (bbox_border(3) and borders(7))) = '1' then
+			layer <= "1000"; -- top level border
+			colour <= C_border1; 
+		elsif (((bbox_h and (player0_dir(0) or player1_dir(0))) or (bbox_v and (not(player0_dir(0) or not(player1_dir(0)))))) and (player0_layer or player1_layer) and (player0_en or player1_en)) = '1' then --this if condition can be made better i think
+			layer <= "0111"; -- top level player
+			if (player0_en = '1') then 
+				colour <= C_player0;
+			else
+				colour <= C_player1;
+			end if;
+		elsif ((bbox_wall(4) and wall_1_N) or (bbox_wall(5) and ((walls(6 downto 4) = "010") or (walls(6 downto 4) = "110") or (walls(6 downto 4) = "101"))) or (bbox_wall(6) and walls(6)) or (bbox_wall(7) and walls(7))) = '1' then
+			layer <= "0110"; -- top level walls
+			--how to know which player made the wall?
+		elsif ((bbox_jump(0) and jumps(4)) or (bbox_jump(1) and jumps(5)) or (bbox_jump(2) and jumps(6)) or (bbox_jump(3) and jumps(7))) = '1' then
+			layer <= "0101"; -- top level jumps
+			colour <= C_jump1;
+		elsif ((bbox_border(0) and borders(0)) or (bbox_border(1) and borders(1)) or (bbox_border(2) and borders(2)) or (bbox_border(3) and borders(3))) = '1' then
+			layer <= "0100"; -- bottom level border
+			colour <= C_border0;
+		elsif (((bbox_h and (player0_dir(0) or player1_dir(0))) or (bbox_v and (not(player0_dir(0) or not(player1_dir(0)))))) and (not(player0_layer) or not(player1_layer))) = '1' then
+			layer <= "0011"; -- bottom level player
+			if (player0_en = '1') then
+				colour <= C_player0;
+			else
+				colour <= C_player1;
+			end if;
+		elsif ((bbox_wall(0) and walls(0)) or (bbox_wall(1) and walls(1)) or (bbox_wall(2) and walls(2)) or (bbox_wall(3) and walls(3))) = '1' then
+			layer <= "0010"; -- bottom level wall
+			-- how to know which player made the wall?
+		elsif ((bbox_jump(0) and jumps(0)) or (bbox_jump(1) and jumps(1)) or (bbox_jump(2) and jumps(2)) or (bbox_jump(3) and jumps(4))) = '1' then
+			layer <= "0001"; -- bottom level jumps
+			colour <= C_jump0;
+		elsif (bbox_dot) = '1' then
+			layer <= "0000"; -- dot
+			colour <= C_dot;
+		else
+			colour <= C_background;
 		end if;
 	end process;
-
 end behaviour;
